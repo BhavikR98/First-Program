c897966c3cdcdaa821b33225cc78027e8e31a109
Shehab Ibrahim <sai6@njit.edu>
2019-02-13 17:05:52 -0500

    cleaning up

--- index.php
+++ index.php
@@ -1,38 +1,4 @@
-<<<<<<< HEAD
-<html>
 
-<head>
-    <title>IS 218 FIRST PROJECT</title>
-</head>
-
-<body>
-    <p>Hello World This Is Our IS 218 First Project</p>
-    <p>Within this project we test a basic function in PHP</p>
-
-
-    <?php
-
-        echo "First Project";
-
-
-
-    ?>
-
-
-</body>
-
-
-<footer>
-    <p>Coded by Bhavik Rana, </p>
-</footer>
-
-</html>
-/**
- * Created by PhpStorm.
- * User: YoungB
- * Date: 2/13/2019
- * Time: 4:51 PM
-=======
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,23 +6,12 @@
     <title>Title</title>
 </head>
 <body>
+<p>Hello World This Is Our IS 218 First Project</p>
+<p>Within this project we test a basic function in PHP</p>
 <?php
-/**
- * Created by PhpStorm.
- * User: shehabibrahim
- * Date: 2019-02-13
- * Time: 16:44
- */
-    echo "hello world";
+
+echo "hello world";
 ?>
 
 </body>
 </html>
-
-/**
- * Created by PhpStorm.
- * User: shehabibrahim
- * Date: 2019-02-13
- * Time: 16:44
->>>>>>> origin/master
- */
