f2821e971b4bd9c4d2b5d6c032e8d43e9f5ca18f
Shehab Ibrahim <sai6@njit.edu>
2019-02-13 16:47:50 -0500

    Adding PHP file

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shehabibrahim
+ * Date: 2019-02-13
+ * Time: 16:44
+ */
+
+    echo "Hello World";
+
+?>
+
+</body>
+</html>
+
+/**
+ * Created by PhpStorm.
+ * User: shehabibrahim
+ * Date: 2019-02-13
+ * Time: 16:44
+ */
