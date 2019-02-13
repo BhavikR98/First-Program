640e6469e77dff6b5f7b6fb452b10cefbad427a6
YoungB <brr23@njit.edu>
2019-02-13 16:58:37 -0500

    Bhav: Added basic index file with some code

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+    <title>IS 218 FIRST PROJECT</title>
+</head>
+
+<body>
+    <p>Hello World This Is Our IS 218 First Project</p>
+    <p>Within this project we test a basic function in PHP</p>
+
+
+    <?php
+
+        echo "First Project";
+
+
+
+    ?>
+
+
+</body>
+
+
+<footer>
+    <p>Coded by Bhavik Rana, </p>
+</footer>
+
+</html>
+/**
+ * Created by PhpStorm.
+ * User: YoungB
+ * Date: 2/13/2019
+ * Time: 4:51 PM
+ */
